b769766f8907c7808bf91ad490dffbb29ee65b71
ITHUB\Student <qew>
2024-09-12 19:59:06 +0300

    feat : add player model and player movement script

---
+++ TPC/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assembly_CSharp
+{
+    public class PlayerMovement
+    {
+        public void Jump(float jumpforse, Rigidbody rb)
+        {
+            rb.AddForce(Vector3.up * jumpforse);
+        }
+
+        public void Move(float moveforse, Rigidbody rb, Vector3 direction)
+        {
+            rb.velocity += direction * moveforse;
+        }
+        
+        public void Rotate(float angle, Rigidbody rb)
+        {
+
+        }
+    }
+}
